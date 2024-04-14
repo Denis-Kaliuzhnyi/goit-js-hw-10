5167f0775eea7c795c0e1c50b9678787a1f61935
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-04-14 17:01:12 +0300

    6

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const makePromise = ({ delay, state }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  const delay = parseInt(form.elements['delay'].value);
+  const state = form.elements['state'].value;
+  makePromise({ delay, state })
+    .then(delay => {
+      iziToast.success({
+        title: 'Notification',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Notification',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight'
+      });
+    });
+});
+
