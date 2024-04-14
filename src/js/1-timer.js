7f9a4f235dde236658124fa97f746821a96410fa
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-04-14 13:54:48 +0300

    5

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,7 +3,6 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-
 let startDate = 0;
 const btnStart = document.querySelector('[data-start]');
 const inputWindow = document.querySelector('#datetime-picker');
@@ -26,7 +25,7 @@ flatpickr('#datetime-picker', {
       selectedDates[0] = Date.now();
       btnStart.disabled = true;
     } else {
-        startDate = selectedDate;
+      startDate = selectedDate;
       btnStart.disabled = false;
     }
   },
@@ -52,10 +51,10 @@ function startCountdown() {
 }
 
 function displayTimerValues(days, hours, minutes, seconds) {
-  daysCountdown.textContent = addLeadingZero(days);
-  hoursCountdown.textContent = addLeadingZero(hours);
-  minutesCountdown.textContent = addLeadingZero(minutes);
-  secondsCountdown.textContent = addLeadingZero(seconds);
+  daysCountdown.textContent = addZero(days);
+  hoursCountdown.textContent = addZero(hours);
+  minutesCountdown.textContent = addZero(minutes);
+  secondsCountdown.textContent = addZero(seconds);
 }
 
 function resetTimerValues() {
@@ -63,20 +62,20 @@ function resetTimerValues() {
 }
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = 60 * second;
-    const hour = 60 * minute;
-    const day = 24 * hour;
-  
-    return {
-      days: Math.floor(ms / day),
-      hours: Math.floor((ms % day) / hour),
-      minutes: Math.floor((ms % hour) / minute),
-      seconds: Math.floor((ms % minute) / second)
-    };
-  }
-  
-const addLeadingZero = value => {
+  const second = 1000;
+  const minute = 60 * second;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+
+  return {
+    days: Math.floor(ms / day),
+    hours: Math.floor((ms % day) / hour),
+    minutes: Math.floor((ms % hour) / minute),
+    seconds: Math.floor((ms % minute) / second),
+  };
+}
+
+const addZero = value => {
   return value.toString().padStart(2, '0');
 };
 
